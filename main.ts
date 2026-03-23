cab12aa3a9809061832e5e432e95eb4060935015
sp1121537-art <sp1121537@spjh.tp.edu.tw>
2026-03-23 11:00:42 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,201 @@
+input.onButtonPressed(Button.A, function () {
+    a += 1
+    while (true) {
+    	
+    }
+    while (true) {
+    	
+    }
+    while (true) {
+    	
+    }
+    while (true) {
+    	
+    }
+    while (true) {
+    	
+    }
+    while (a == 6) {
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 7) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 8) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 9) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 11) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 12) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 13) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 14) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 15) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (a == 16) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    while (a == 17) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    while (a == 18) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    }
+    while (a == 19) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    while (a == 20) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            `)
+    }
+    while (a == 21) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    while (a == 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            `)
+    }
+    while (a == 23) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    }
+    while (a == 24) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            `)
+    }
+    while (a == 25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+})
+let a = 0
+basic.clearScreen()
+a = 0
